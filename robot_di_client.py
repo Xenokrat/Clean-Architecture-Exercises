40c07284aa9a157940a8af61a12e58d8901ff380
Pavel Barabanov <p.barabanov@definnovation.com>
2025-03-12 19:38:24 +0300

    feat: lesson on DI

---
+++ robot_di_client.py
@@ -0,0 +1,31 @@
+from robot_di_server import CleanerApi
+import pure_robot 
+
+def transfer_to_cleaner(message):
+    print(message)
+
+# Вставка завпсимостей
+cleaner_api = CleanerApi(
+    robot_cls     = pure_robot.RobotState,
+    position_x    = 0,
+    position_y    = 0,
+    angle         = 0.0,
+    initial_state = pure_robot.WATER,
+    transfer      = transfer_to_cleaner,
+)
+
+cleaner_api.activate_cleaner((
+    'move 100',
+    'turn -90',
+    'set soap',
+    'start',
+    'move 50',
+    'stop'
+    ))
+
+print(
+    cleaner_api.get_x(), 
+    cleaner_api.get_y(), 
+    cleaner_api.get_angle(), 
+    cleaner_api.get_state()
+)
---
+++ robot_di_server.py
@@ -0,0 +1,65 @@
+from typing import Callable
+import pure_robot
+
+type Transfer = Callable[[str], None]
+
+# класс Чистильщик API
+class CleanerApi:
+
+    # конструктор
+    # расширим его параметрами
+    def __init__(
+        self,
+        robot_cls:     pure_robot.RobotState,
+        position_x:    int,
+        position_y:    int,
+        angle:         float,
+        initial_state: int,
+        transfer:      Transfer
+    ):
+        self.cleaner_state = robot_cls(
+            position_x,
+            position_y,
+            angle,
+            initial_state,
+        )
+        self.transfer = transfer
+
+    def get_x(self):
+        return self.cleaner_state.x
+
+    def get_y(self):
+        return self.cleaner_state.y
+
+    def get_angle(self):
+        return self.cleaner_state.angle
+
+    def get_state(self):
+        return self.cleaner_state.state
+
+    def activate_cleaner(self,code):
+        for command in code:
+            cmd = command.split(' ')
+            if cmd[0]=='move':
+                self.cleaner_state = pure_robot.move(
+                    self.transfer,
+                    int(cmd[1]),
+                    self.cleaner_state) 
+            elif cmd[0]=='turn':
+                self.cleaner_state = pure_robot.turn(
+                    self.transfer,
+                    int(cmd[1]),
+                    self.cleaner_state)
+            elif cmd[0]=='set':
+                self.cleaner_state = pure_robot.set_state(
+                    self.transfer,
+                    cmd[1],
+                    self.cleaner_state) 
+            elif cmd[0]=='start':
+                self.cleaner_state = pure_robot.start(
+                    self.transfer,
+                    self.cleaner_state)
+            elif cmd[0]=='stop':
+                self.cleaner_state = pure_robot.stop(
+                    self.transfer,
+                    self.cleaner_state)
